7301066f968a47f539d2256be2e93f03e65ef488
vincentpham <56554113+vincentpham@users.noreply.github.com>
2023-08-16 19:15:18 -0400

    just starting some work

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
 # reflect an existing database into a new model
+
 
 # reflect the tables
 
 
 # Save references to each table
-
+session = Session(engine)
 
 # Create our session (link) from Python to the DB
 
@@ -21,7 +26,7 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
