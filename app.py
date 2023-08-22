d18ccd59103afdd6793b912f89d3108e3eabdca4
vincentpham <vincent.pham@live.ca>
2023-08-21 22:35:31 -0400

    .

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import and_ # to join tables
+from sqlalchemy.orm import sessionmaker
 import datetime as dt # in case i need to convert date time formatting
 
 
@@ -22,6 +23,7 @@ Station = Base.classes.station
 # Save references to each table 
 # Create our session (link) from Python to the DB
 session = Session(engine)
+Session = sessionmaker(bind=engine)
 
 
 #################################################
@@ -35,104 +37,90 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 @app.route("/")
-
-# date as the key
-# prcp as the value
-
-@app.route("/api/v1.0/<start>")
-
-# def start(start):
-    # try:
-       #  dt.datetime.strptime(start, '%Y-%m-%d')
-        # results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
-        # data = {"TMIN": results[0][0], "TAVG": results[0][1], "TMAX": results[0][2]}
-       #  return jsonify(data)
-    # except ValueError:
-       #  return jsonify({"error": "Invalid date format. Please use 'YYYY-MM-DD'."}), 400
-
 def home():
-    return(
+    return (
         f"Welcome! Here are the available routes<br/>"
         f"<a href='/api/v1.0/precipitation'>/api/v1.0/precipitation</a><br/>"
         f"<a href='/api/v1.0/stations'>/api/v1.0/stations</a><br/>"
         f"<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br/>"
         f"<a href='/api/v1.0/<start>'>/api/v1.0/{{start_date}}</a><br/>"
         f"<a href='/api/v1.0/<start>/<end>'>/api/v1.0/{{start_date}}/{{end_date}}</a><br/>"
-            )
-    
-# precipitation route
+    )
+
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    session = Session()
+    
     recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
     start_date = dt.datetime.strptime(recent_date, '%Y-%m-%d') - dt.timedelta(days=365)
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= start_date).all()
+    data = {date: prcp for date, prcp in results}
     
-    data = {}
-    for date, prcp in results:
-        data[date] = prcp
-
+    session.close()
     return jsonify(data)
 
-# stations route
-
-# okay only stations route is working so far
 
 @app.route("/api/v1.0/stations")
 def stations():
+    session = Session()
+    
     results = session.query(Station.station).all()
-    
-    data = []
-    
-    for x in results:
-        data.append(x[0])
-        
+    data = [x[0] for x in results]
+    session.close()
     return jsonify(data)
 
-# temperature observation stations route
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    session = Session()
+    most_active_station_id = session.query(Measurement.station).group_by(Measurement.station).order_by(func.count().desc()).first()[0]
+    recent_date = session.query(Measurement.date).filter(Measurement.station == most_active_station_id).order_by(Measurement.date.desc()).first()[0]
+    start_date = dt.datetime.strptime(recent_date, '%Y-%m-%d') - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).filter(Measurement.station == most_active_station_id).filter(Measurement.date >= start_date).all()
+    data = [temp[0] for temp in results]
+    session.close()
+    return jsonify(data)
+
 # @app.route("/api/v1.0/tobs")
 # def tobs():
+    ## most active station ID
     # most_active_station_id = session.query(Measurement.station).group_by(Measurement.station).order_by(func.count().desc()).first()[0]
     # recent_date = session.query(Measurement.date).filter(Measurement.station == most_active_station_id).order_by(Measurement.date.desc()).first()[0]
     # start_date = dt.datetime.strptime(recent_date, '%Y-%m-%d') - dt.timedelta(days=365)
     
-    # results = session.query(Measurement.tobs).filter(Measurement.station == most_active_station_id).filter(Measurement.date >= start_date).all()
+    # joining measurement and station tables
+    # results = session.query(Measurement.tobs, Station.name, Station.latitude, Station.longitude).\
+        # join(Station, Measurement.station == Station.station).\
+        # filter(and_(Measurement.station == most_active_station_id, Measurement.date >= start_date)).all()
 
-    # data = []
-    # for temp in results:
-        # data.append(temp[0])
+    # # processing the data as needed
+    # data = [{"Temperature": temp, "Station Name": name, "Latitude": lat, "Longitude": lon} for temp, name, lat, lon in results]
 
     # return jsonify(data)
-
-@app.route("/api/v1.0/tobs")
-def tobs():
-    # most active station ID
-    most_active_station_id = session.query(Measurement.station).group_by(Measurement.station).order_by(func.count().desc()).first()[0]
-    recent_date = session.query(Measurement.date).filter(Measurement.station == most_active_station_id).order_by(Measurement.date.desc()).first()[0]
-    start_date = dt.datetime.strptime(recent_date, '%Y-%m-%d') - dt.timedelta(days=365)
-    
-    # joining measurement and station tables
-    results = session.query(Measurement.tobs, Station.name, Station.latitude, Station.longitude).\
-        join(Station, Measurement.station == Station.station).\
-        filter(and_(Measurement.station == most_active_station_id, Measurement.date >= start_date)).all()
-
-    # processing the data as needed
-    data = [{"Temperature": temp, "Station Name": name, "Latitude": lat, "Longitude": lon} for temp, name, lat, lon in results]
-
-    return jsonify(data)
 
 
 # start and end routes
 
 @app.route("/api/v1.0/<start>")
 def start(start):
+    session = Session()
+    
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
     data = {"Temperature Minimum": results[0][0], "Temperature Average": results[0][1], "Temeprature Max": results[0][2]}
+    
+    session.close()
     return jsonify(data)
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
+    session = Session()
+    
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-    data = {"TMIN": results[0][0], "TAVG": results[0][1], "TMAX": results[0][2]}
+    data = {"Temperature Minimum": results[0][0], "Temperature Average": results[0][1], "Temperature Max": results[0][2]}
+    
+    session.close()
     return jsonify(data)
 
 # Starting the app
